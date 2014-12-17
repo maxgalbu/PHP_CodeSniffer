a764ca2ce77c088f17dba6d4b078b3e10f3b1659
Max Galbusera <max.galbusera@gmail.com>
2014-12-17 11:16:57 +0100

    add my standards

---
+++ CodeSniffer/Standards/MJL/Sniffs/WhiteSpace/ObjectOperatorSpacingSniff.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * MJL_Sniffs_WhiteSpace_ObjectOperatorSpacingSniff.
+ *
+ * PHP version 5
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006-2014 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+
+/**
+ * MJL_Sniffs_WhiteSpace_ObjectOperatorSpacingSniff.
+ *
+ * Ensure there is no whitespace before a semicolon.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Greg Sherwood <gsherwood@squiz.net>
+ * @author    Marc McIntyre <mmcintyre@squiz.net>
+ * @copyright 2006-2014 Squiz Pty Ltd (ABN 77 084 670 600)
+ * @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/PHP_CodeSniffer
+ */
+class MJL_Sniffs_WhiteSpace_ObjectOperatorSpacingSniff implements PHP_CodeSniffer_Sniff
+{
+
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return array(T_OBJECT_OPERATOR);
+
+    }//end register()
+
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
+     * @param int                  $stackPtr  The position of the current token
+     *                                        in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+    
+        //Allow whitespace if the object operator is on a newline
+        $newlineFound = false;
+        $index = 1;
+        do {
+            $prevType = $tokens[($stackPtr - $index)]['code'];
+            $prevChar = $tokens[($stackPtr - $index)]['content'];
+            if (strpos($prevChar, "\n") !== false) {
+                $newlineFound = true;
+                break;
+            }
+            
+            $index++;
+        } while (in_array($prevType, PHP_CodeSniffer_Tokens::$emptyTokens) === true);
+        
+        if (!$newlineFound) {
+            $prevType = $tokens[($stackPtr - 1)]['code'];
+            if (in_array($prevType, PHP_CodeSniffer_Tokens::$emptyTokens) === true) {
+                $error = 'Space found before object operator';
+                $phpcsFile->addError($error, $stackPtr, 'Before');
+            }
+        }
+
+        $nextType = $tokens[($stackPtr + 1)]['code'];
+        if (in_array($nextType, PHP_CodeSniffer_Tokens::$emptyTokens) === true) {
+            $error = 'Space found after object operator';
+            $phpcsFile->addError($error, $stackPtr, 'After');
+        }
+
+    }//end process()
+
+
+}//end class
+
+?>
---
+++ CodeSniffer/Standards/MJL/Sniffs/WhiteSpace/ScopeClosingBraceSniff.php
@@ -0,0 +1,9 @@
+<?
+	class MJL_Sniffs_WhiteSpace_ScopeClosingBraceSniff extends Squiz_Sniffs_WhiteSpace_ScopeClosingBraceSniff
+	{
+		public $supportedTokenizers = array(
+			'PHP',
+			'JS',
+		);
+	}
+?>
